fa36a8a4ee229539cc816f321628d4298a6225d9
PriscillaRenault <priscilla.renault72460@gmail.com>
2023-12-19 13:34:03 +0100

    navbar burger and desktop OK on header but not on all the pages

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
+// burger Menu
 const openBurger = document.getElementById('js-Open')
 const closeBurger = document.getElementById('js-Close')
 const menuList = document.getElementById('js-menuList')
 
+//Show Desktop Menu
+window.addEventListener('resize', function () {
+  const width = window.innerWidth
+  if (width > 650) {
+    menuList.classList.remove('inactive')
+    openBurger.classList.add('inactive')
+  } else {
+    menuList.classList.add('inactive')
+    openBurger.classList.remove('inactive')
+  }
+})
+//open burger Menu
 openBurger.addEventListener('click', () => {
   closeBurger.classList.remove('inactive')
   menuList.classList.remove('inactive')
   openBurger.classList.add('inactive')
 })
 
+//close burger Menu
 closeBurger.addEventListener('click', () => {
   closeBurger.classList.add('inactive')
   menuList.classList.add('inactive')
