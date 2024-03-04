04bbb9fa9261f37aa1e988244f9a6f4c57d3661c
PriscillaRenault <priscilla.renault72460@gmail.com>
2024-03-04 16:01:04 +0100

    js pilots ok scale and switch reste transition

--- JS/pilots.js
+++ JS/pilots.js
@@ -2,20 +2,39 @@ let card = document.querySelectorAll('.flipCard')
 let frontCard = document.querySelectorAll('.js-front')
 let backCard = document.querySelectorAll('.js-back')
 
-function changeCard() {
+function changeCard(actionEnter, actionExit) {
     for (let i = 0; i < card.length; i++) {
-        card[i].addEventListener('mouseenter', (event) => {
-            backCard[i].classList.remove('hidden')
+        card[i].addEventListener(actionEnter, () => {
             frontCard[i].classList.remove('flipCard__front')
             frontCard[i].classList.add('hidden')
-            console.log(frontCard[i])
+            backCard[i].classList.remove('hidden')
+            backCard[i].addEventListener('click', () => {
+                if (backCard[i].classList.contains('zoomed')) {
+                    backCard[i].classList.remove('zoomed')
+                } else {
+                    backCard[i].classList.add('zoomed')
+                }
+            })
         })
-        card[i].addEventListener('mouseleave', (event) => {
+        card[i].addEventListener(actionExit, () => {
             backCard[i].classList.add('hidden')
+            backCard[i].classList.remove('zoomed')
             frontCard[i].classList.add('flipCard__front')
             frontCard[i].classList.remove('hidden')
         })
     }
+}
+
+function zoom() {
+    backCard.addEventListener('click', () => {
+        backCard.classList.add('zoomed')
+    })
+}
+
+if (window.screen.width <= 768) {
+    changeCard('click', 'click')
+} else {
+    changeCard('mouseenter', 'mouseleave')
 }
 
 changeCard()
